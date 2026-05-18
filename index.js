b97bae4216340e0bd62e84412ea146284d4230bc
fahad1shakil <fahad1shakil@gmail.com>
2026-05-19 00:30:02 +0600

    New folder setup

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Basic Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:5174', 'https://localchefbazzar-fahadshakil.netlify.app'],
+  origin: "*",
   credentials: true
 }));
 app.use(express.json());
